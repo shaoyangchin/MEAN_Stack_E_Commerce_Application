99e5f1d1d9b0e65c0ea10327fdb07df203e55353
shaoyangchin <84553506+shaoyangchin@users.noreply.github.com>
2021-08-02 16:38:12 +0800

    initial commit

---
+++ apps/ngshop/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'munch-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ngshop';
+}
