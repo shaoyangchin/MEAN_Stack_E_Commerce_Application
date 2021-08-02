d593026b738849ddf7ec71bac547447d1d0f379b
shaoyangchin <84553506+shaoyangchin@users.noreply.github.com>
2021-08-02 19:38:52 +0800

    first setup commit

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HomePageComponent, ProductListComponent],
+  imports: [BrowserModule, RouterModule.forRoot([{
+    path: "",
+    component: HomePageComponent,
+  },
+{
+  path: "products", component: ProductListComponent
+}])],
   providers: [],
   bootstrap: [AppComponent],
 })
